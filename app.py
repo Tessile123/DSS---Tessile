84e43f26cbf338ad879ca36bdd9bd6802170f8d3
Alberto <alberto.capuzzoni@gmail.com>
2026-06-09 13:52:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if "passaporto" in params:
             .c2 { left: 25%; animation-duration: 3.5s; animation-delay: 2s; }
             .c3 { left: 40%; animation-duration: 3.5s; animation-delay: 1s; }
             .c4 { left: 55%; animation-duration: 4.5s; animation-delay: 2.8s; }
-            .c5 { left: 70%; animation-duration: 3.2s; animation-delay: 1.5s; }
+            .c5 { left: 70%; animation-duration: 3.5s; animation-delay: 1.5s; }
             .c6 { left: 85%; animation-duration: 3.5s; animation-delay: 0.5s; }
             .c7 { left: 85%; animation-duration: 3.2s; animation-delay: 2.5s; }
             </style>
